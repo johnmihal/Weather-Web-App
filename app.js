55e85a1a06c7c319922bbef7f601ee739f42d84c
John Mihal <jm7198@rit.edu>
2022-06-17 11:47:22 -0700

    adding photo api stuff

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const request = require('request')
+const express = require('express')
+const app = express()
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.set('view engine', 'ejs')
+
+const request = require('request');
 const dotenv = require('dotenv').config()
 
-const address = process.argv[2]
-
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&units=imperial&appid=${process.env.API_KEY}`
-
-if (!address){
-  return console.log('Please enter: city, country')
-}
-
-request(url, (error, response, body) => {
-  const data = JSON.parse(body)
-  console.log(`It's currently ${data.main.temp} outside.`)
+app.route("/")
+  .get((req,res) => {
+    res.render('index.ejs')
+  })
+  .post((req,res) => {
+    let address = req.body.address
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&units=imperial&appid=${process.env.WEATHER_API_KEY}`
+    if (!address){
+      res.render('index.ejs', {address: req.body.address, temp: ''})
+    } else {
+      request(url, (error, response, body) => {
+        const data = JSON.parse(body)
+        res.render('index.ejs', {address: req.body.address, temp: data.main.temp})
+      })
+    } 
 })
+
+
+app.listen(3000)
