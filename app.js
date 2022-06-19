e4cb64ca26cb5c8fb7406971daed497f0c098c08
John Mihal <jm7198@rit.edu>
2022-06-19 14:37:11 -0700

    im just gonna partially restart lol

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 const express = require('express')
 const app = express()
 app.use(express.static("public"))
-app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.set('view engine', 'ejs')
@@ -9,11 +13,15 @@ app.set('view engine', 'ejs')
 const request = require('request');
 const dotenv = require('dotenv').config()
 
-app.route("/")
+app.route("/weather")
+/*
   .get((req,res) => {
     res.render('index.ejs')
   })
+  */
   .post((req,res) => {
+    console.log(req.body)
+ 
     let address = req.body.address
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&units=imperial&appid=${process.env.WEATHER_API_KEY}`
     if (!address){
@@ -27,4 +35,6 @@ app.route("/")
 })
 
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Server Started')
+})
